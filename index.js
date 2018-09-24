3a8a2b852964cf48fa180d6738b06b367868bd70
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-24 16:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function happyHolidays(string) {
     return `${"Happy holidays!"}`
   }
+
+function sayhappyHolidaysTo(name) {
+    return "Happy holidays, ${name}!"
+  if (name === you) {
+    return ('Happy holidays, you!'); }
+}
