138310ece5b658393e14e4980ce7eedd62217f9e
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-09-24 17:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function happyHolidayTo(string) {
 
 function holidayCountdown(string) {
     console.log `"It\'s ${"days"} until ${"holiday"}!`
-    return ('It\'s 20 days days until Mother\'s Day'!)
+    return ('It\'s 20 days days until Mother\'s Day!')
 }
